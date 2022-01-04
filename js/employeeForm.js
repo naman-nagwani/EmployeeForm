2852c7b47d7b17b3c9ec90970d3152591148e0f1
ToodlyDave <alapatdavid@gmail.com>
2022-01-04 11:55:32 +0530

    ADD ui cues for invalid date

--- js/employeeForm.js
+++ js/employeeForm.js
@@ -3,9 +3,8 @@ let employee = new Employee;
 window.addEventListener("load", () => {
     let salary = document.querySelector("salary");
     let salaryValue = document.querySelector("#salary");
-    
     let name = document.querySelector("#name");
-    
+
     salaryValue.addEventListener("input", () => {
         salary.textContent = salaryValue.value;
     })
@@ -23,40 +22,40 @@ window.addEventListener("load", () => {
 
 function saveForm() {
 
-    
-
-    try {
-        let profile = document.querySelector('input[name="profile-choice"]:checked');
-        if (profile != null) {
-            employee.profilePic = profile.value;
-        }
-
-        let gender = document.querySelector('input[name="gender"]:checked');
-        if (gender != null) {
-            employee.gender = gender.value;
-        }
-
-        employee.salary = document.querySelector('#salary').value;
-        employee.notes = document.querySelector('#notes').value;
-
-        let day = document.querySelector('select[name=Day]').value;
-        let month = document.querySelector('select[name=Month]').value;
-        let year = document.querySelector('select[name=Year]').value;
-
-        employee.startDate = day + "-" + month + "-" + year;
-
-        let department = [];
-        document.getElementsByName('department').forEach( (element) => {
-            if (element.checked == true) {
-                department.push(element.value)
-            }
-        });
-        employee.department = department;        
-
-    } catch (e) {
-        
-        console.log(e);
+    let profile = document.querySelector('input[name="profile-choice"]:checked');
+    if (profile != null) {
+        employee.profilePic = profile.value;
     }
+
+    let gender = document.querySelector('input[name="gender"]:checked');
+    if (gender != null) {
+        employee.gender = gender.value;
+    }
+
+    employee.salary = document.querySelector('#salary').value;
+    employee.notes = document.querySelector('#notes').value;
+    
+    let day = document.querySelector('select[name=Day]').value;
+    let month = document.querySelector('select[name=Month]').value;
+    let year = document.querySelector('select[name=Year]').value;
+    let errorDate = document.querySelector("#error-date");
+    console.log(day + "-" + month + "-" + year);  
+    try {
+        employee.startDate = day + "-" + month + "-" + year;  
+        errorDate.textContent = "";
+    } catch (invalidDate) {
+        errorDate.textContent = invalidDate;
+    }
+
+    let department = [];
+    document.getElementsByName('department').forEach( (element) => {
+        if (element.checked == true) {
+            department.push(element.value)
+        }
+    });
+    employee.department = department;        
+
+    
 
     console.log(" saved " + JSON.stringify(employee));
     
