2a14a4f232a41e5fcc4a8b6fe36f514b5be40f5b
ToodlyDave <alapatdavid@gmail.com>
2022-01-06 22:28:20 +0530

    UPDATE table to read from json

--- js/home.js
+++ js/home.js
@@ -19,26 +19,59 @@ const createHomeHtml = () => {
     
     let innerHtml = `${headerInnerHtml}`;
 
-    innerHtml +=  `
-    <tr>
-        <td></td>
-        <td>David</td>
-        <td>Male</td>
-        <td>
-            IT
-            HR
-        </td>
-        <td>12456789</td>
-        <td>01 Jan 2022</td>
-        <td>
-            <i class="material-icons">delete</i>
-            <i class="material-icons">edit</i>
-        </td>
-    </tr>`;
-    
+    for (const emp of dummyJson.employeeData) {
+        
+        innerHtml +=  `
+        <tr>
+            <td></td>
+            <td>${emp._name}</td>
+            <td>${emp._gender}</td>
+            <td>
+                <span class="department-chip">${emp._department[0]}</span>
+                <span class="department-chip">${emp._department[1]}</span>
+            </td>
+            <td>${emp._salary}</td>
+            <td>${emp._startDate}</td>
+            <td>
+                <i class="material-icons">delete</i>
+                <i class="material-icons">edit</i>
+            </td>
+        </tr>`;
+        
+    }
+
     document.querySelector("#employee-table").innerHTML = innerHtml;
 }
 
 const addUser = () => {
     window.location = "./employeeForm.html";
+}
+
+const dummyJson = {
+    "employeeData": [
+        {
+            "_name": "David B Alapat",
+            "_gender": "M",
+            "_department": [
+              "Admin",
+              "HR"
+            ],
+            "_salary": 120000,
+            "_startDate": "03-01-2022",
+            "_profilePic": "Ellipse-1",
+            "id": 1
+          },
+          {
+            "_name": "John Doe",
+            "_gender": "M",
+            "_department": [
+              "IT",
+              "HR"
+            ],
+            "_salary": 100000,
+            "_startDate": "02-01-2022",
+            "_profilePic": "Ellipse-2",
+            "id": 2
+          },
+    ]
 }
