fab3d8a62c25cf476b53dab9bef0bbf34ccefbed
ToodlyDave <alapatdavid@gmail.com>
2022-01-06 22:21:38 +0530

    ADD js file to populate the table

--- js/home.js
+++ js/home.js
@@ -1,3 +1,44 @@
+window.addEventListener("load", () => {
+
+    createHomeHtml();
+})
+
+const createHomeHtml = () => {
+
+    const headerInnerHtml = `
+    <tr>
+        <th></th>
+        <th>NAME</th>
+        <th>GENDER</th>
+        <th>DEPARTMENT</th>
+        <th>SALARY</th>
+        <th>START DATE</th>
+        <th>ACTIONS</th>
+    </tr>`;
+
+    
+    let innerHtml = `${headerInnerHtml}`;
+
+    innerHtml +=  `
+    <tr>
+        <td></td>
+        <td>David</td>
+        <td>Male</td>
+        <td>
+            IT
+            HR
+        </td>
+        <td>12456789</td>
+        <td>01 Jan 2022</td>
+        <td>
+            <i class="material-icons">delete</i>
+            <i class="material-icons">edit</i>
+        </td>
+    </tr>`;
+    
+    document.querySelector("#employee-table").innerHTML = innerHtml;
+}
+
 const addUser = () => {
     window.location = "./employeeForm.html";
 }
