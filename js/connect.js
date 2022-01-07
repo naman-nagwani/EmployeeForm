3a31724eaa2ae72646a5473c3c2040ecbe169f6d
ToodlyDave <alapatdavid@gmail.com>
2022-01-07 10:41:57 +0530

    UPDATE ajax method to use promises

--- js/connect.js
+++ js/connect.js
@@ -1,33 +1,37 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const makeAjaxCall = (val, method, url, data=null, async=true) => {
-    let xml = new XMLHttpRequest();
 
-    xml.onreadystatechange = () => {
-        console.log(" (" + val + ") ready state: " + xml.readyState + " status: " + xml.status);
-        if (xml.readyState == 4) {
-            if (xml.status.toString().match("^[2][0-9]{2}$")) {
-                console.log(" (" + val + ") the response: ");
-                console.log(xml.responseText);
-            }
-            else {
-                console.log(" (" + val + ") ERROR: " + xml.status);
+    return new Promise( (resolve, reject) => {
+        
+        let xml = new XMLHttpRequest();
+        
+        xml.onreadystatechange = () => {
+            console.log(" (" + val + ") " + method + " ready state: " + xml.readyState + " status: " + xml.status);
+            if (xml.readyState == 4) {
+                if (xml.status.toString().match("^[2][0-9]{2}$")) {
+                    resolve(" (" + val + ") the response: " + xml.responseText)
+                }
+                else {
+                    reject(" (" + val + ") ERROR: " + xml.status)
+                    // console.log(" (" + val + ") ERROR: " + xml.status);
+                }
             }
         }
-    }
-
-    xml.open(method, url, async);
-    if (data) {
-        console.log(" (" + val + ") we have data to deal with");
-        xml.setRequestHeader("Content-Type", "Application/json")
-        console.log(" (" + val + ") sent data request");
-        xml.send(JSON.stringify(data));
-    }
-    else {
-        xml.send();
-        console.log(" (" + val + ") sent request");
-    }
-
+        
+        xml.open(method, url, async);
+        if (data) {
+            console.log(" (" + val + ") we have data to deal with");
+            xml.setRequestHeader("Content-Type", "Application/json")
+            console.log(" (" + val + ") sent data request");
+            xml.send(JSON.stringify(data));
+        }
+        else {
+            xml.send();
+            console.log(" (" + val + ") sent request");
+        }
+        
+    })
 }
 
 const getURL = "http://localhost:3000/employees";
@@ -43,18 +47,6 @@ newEmployeeData = {
     "Salary": 4789
 }
 
-console.log(" calling post");
-makeAjaxCall(1, "POST", postURL, newEmployeeData);
-
-console.log("\n\n\n calling get to see changes");
-makeAjaxCall(2, "GET", getURL);
-
-console.log("\n\n\n calling delete");
-makeAjaxCall(3, "DELETE", deleteURL + 1);
-
-console.log("\n\n\n calling get to see delete changes");
-makeAjaxCall(4, "GET", getURL);
-
 console.log(" create update emp dummy");
 updateEmployeeData = {
     "Name": "Harry James Potter",
@@ -62,8 +54,57 @@ updateEmployeeData = {
     "Salary": 4789
 }
 
+console.log(" calling post first");
+makeAjaxCall(1, "POST", postURL, newEmployeeData)
+    .then( (response) => {
+        console.log(" posted ");
+        console.log(response);
+        console.log("\n\n\n");
+    })
+    .catch( (rejected) => {
+        console.log(" promise rejected");
+        console.log(rejected);
+    });
 
-setTimeout(() => {
-    console.log(" calling put");
-    makeAjaxCall(5, "PUT", updateURL + 3, updateEmployeeData);
-}, 3000);
+console.log("\n\n\n calling get to see changes");
+makeAjaxCall(2, "GET", getURL)
+    .then( (response) => {
+        console.log(" got");
+        console.log(response);
+        console.log("\n\n\n");
+    })
+    .catch( (rejected) => {
+        console.log(rejected);
+    });
+
+console.log("\n\n\n calling delete");
+makeAjaxCall(3, "DELETE", deleteURL + 1)
+    .then( (response) => {
+        console.log(" deleted");
+        console.log(response);
+        console.log("\n\n\n");
+    })
+    .catch( (rejected) => {
+        console.log(rejected);
+    });
+
+console.log("\n\n\n calling get to see delete changes");
+makeAjaxCall(4, "GET", getURL)
+    .then( (response) => {
+        console.log(" got");
+        console.log(response);
+        console.log("\n\n\n");
+    })
+    .catch( (rejected) => {
+        console.log(rejected);
+    });
+
+makeAjaxCall(5, "PUT", updateURL + 3, updateEmployeeData)
+    .then( (response) => {
+        console.log(" updated");
+        console.log(response);
+        console.log("\n\n\n");
+    })
+    .catch( (rejected) => {
+        console.log(rejected);
+    });
